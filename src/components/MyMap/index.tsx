0cfa425c4f0b77b3c4dc184dff6902e910c48c10
Kinda Hall <1142704468@qq.com>
2022-10-25 20:28:45 +0800

    feat: 🎸 完善高德地图组件

--- src/components/MyMap/index.tsx
+++ src/components/MyMap/index.tsx
@@ -11,14 +11,12 @@ interface MapProps {
 
 export default function MyMap(props: MapProps) {
     const { latitude, longitude, address, zoom } = props;
-    let src: string;
-    src = `https://m.amap.com/navi/?dest=116.470098,39.992838&destName=%E9%98%9C%E9%80%9A%E8%A5%BF&hideRouteIcon=1&key=3aa547ab4b94883184ff59510aec160e`
     return (
         <BrowserOnly>
             {() => {
                 return (
                     <div className={styles.myMapContainer}>
-                        <iframe src="https://m.amap.com/navi/?dest=116.470098,39.992838&destName=%E9%98%9C%E9%80%9A%E8%A5%BF&hideRouteIcon=1&key=3aa547ab4b94883184ff59510aec160e" frameBorder='0' />
+                        <iframe className={styles.mapIframe} src={`https://m.amap.com/navi/?dest=${latitude},${longitude}&destName=${address}&hideRouteIcon=1&key=${API_KEY}`} frameBorder='0' />
                     </div>
                 )
             }
