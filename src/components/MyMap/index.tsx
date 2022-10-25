6b8ecf8dd1b0612aecac997b1fbe411b0900fb98
华勤达 <hqd01334772@antgroup.com>
2022-10-25 18:35:46 +0800

    feat: 🎸 增加一种内嵌map的方式

--- src/components/MyMap/index.tsx
+++ src/components/MyMap/index.tsx
@@ -1,10 +1,7 @@
 import { API_KEY } from '../../common/constant';
 import React, { useState, useEffect } from 'react';
-import { Map, APILoader, Marker } from '@uiw/react-amap';
 import styles from './styles.module.css';
 import BrowserOnly from '@docusaurus/BrowserOnly';
-
-
 interface MapProps {
     latitude: number;
     longitude: number;
@@ -14,32 +11,18 @@ interface MapProps {
 
 export default function MyMap(props: MapProps) {
     const { latitude, longitude, address, zoom } = props;
-    const [location, setLocation] = useState<any>()
-
-    useEffect(() => {
-        // 考虑到AMap加载延迟，此处加一个setTimeOut，使脚本加载在宏任务队列之后
-        setTimeout(() => {
-            setLocation(new AMap.LngLat(latitude, longitude))
-        }, 500);
-
-    }, [props]);
-
+    let src: string;
+    src = `https://m.amap.com/navi/?dest=116.470098,39.992838&destName=%E9%98%9C%E9%80%9A%E8%A5%BF&hideRouteIcon=1&key=3aa547ab4b94883184ff59510aec160e`
     return (
-        <BrowserOnly fallback={<div></div>}>
+        <BrowserOnly>
             {() => {
                 return (
                     <div className={styles.myMapContainer}>
-                        <APILoader akay={API_KEY}>
-                            <Map
-                                center={[latitude, longitude]}
-                                zoom={zoom}
-                            >
-                                <Marker visiable={true} title={address} position={location} />
-                            </Map>
-                        </APILoader>
-                    </div >
+                        <iframe src="https://m.amap.com/navi/?dest=116.470098,39.992838&destName=%E9%98%9C%E9%80%9A%E8%A5%BF&hideRouteIcon=1&key=3aa547ab4b94883184ff59510aec160e" frameBorder='0' />
+                    </div>
                 )
-            }}
-        </BrowserOnly>
+            }
+            }
+        </BrowserOnly >
     )
 }
